2e94ab8b0972b7ef9be36ec669c48a67dcefac5b
Matheus Alefe Munari Zanardi <matheus.orvati@hotmail.com>
2021-08-06 12:04:44 -0300

    iniciar sistema de dados profissionais

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,26 @@
-def profissional(atividade):
+def profissional(): #Dados relacionados ao emprego na iPlace
     atividade = int(input('''Qual tipo de atividade vocês gostaria de realizar?:
-    [0]
+    [0] Organização horária
+    [1] Relatório de vendas comissionadas
     :'''))
+    if atividade not in(0, 1, 2):
+        while atividade not in(0, 1, 2):
+            atividade = int(input('''Resposta inválida. Qual tipo de atividade vocês gostaria de realizar?:
+                [0] Organização horária
+                [1] Relatório de vendas comissionadas
+                :'''))
+    elif atividade == 0:
+        #Cadastro de horários
+        turno = str(input('Qual seu turno atual? [M/I/N]: ')).upper().strip()
+        if turno not in('M', 'I', 'N'): #M = 10:00-18:20, I = 12:00 - 20:20, N = 13:40 - 22:00
+            while turno not in('M', 'I', 'N'):
+                turno = str(input('Resposta Inválida. Qual seu turno atual? [M/I/N]: ')).upper().strip()
+        print('Turno cadastrado com sucesso!')
+    elif atividade == 1:
+        #Cadastro de vendas
+        produto = int(input('''Qual o tipo do produto a ser cadastrado?
+        [0] Apple
+        [1] Acessórios (iPlace ou Outras marcas que não Apple)
+        [2] Serviço
+        :'''))
+#Continuar sistema de vendas comissionadas
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from funcoes import *
 print('=-'*50)
 print('Bem vindo ao S.A.M.A! Sistema de Atividades Matheus Alefe')
 print('=-'*50)
@@ -12,10 +13,12 @@ if escolha not in(0, 1, 2, 3, 9):
     while escolha not in(0, 1, 2, 3, 9):
         escolha = int(input('''Resposta inválida. Qual tipo de atividade gostaria de registrar?
         [0] Horária
-        [1] profissional
+        [1] Profissional
         [2] Estudantil
         [3] Doméstica
         [9] Cancelar Operação
         :'''))
+elif escolha == 1:
+    profissional()
 elif escolha == 9:
     print('Obrigado por usar o nosso sistema!')
